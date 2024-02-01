f8e99356fb280d563cc2f966daf7540b9b37d0db
lchesn2 <lchesn2@wgu.edu>
2024-02-01 10:28:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+st.set_page_config(
+  page_title="The Lars Bars",
+  page_icon="",
+  layout="wide",
+  initial_sidebar_state="expanded")
 
-st.write("Yo Diggity")
+alt.themes.enable("dark")
+
+
+
+with st.sidebar:
+  st.title('nm man')
+  
